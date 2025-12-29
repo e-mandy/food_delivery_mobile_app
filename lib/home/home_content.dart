d2fabd6eea1b7377c6b67f7d836c022c39a5d3b3
e-mandy <andymfrd02@gmail.com>
2025-12-30 00:07:13 +0100

    Mise en place de la section d'affichage des différents repas

--- lib/home/home_content.dart
+++ lib/home/home_content.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_mobile_app/home/components/category.dart';
 import 'package:food_delivery_mobile_app/home/components/delivery.dart';
+import 'package:food_delivery_mobile_app/home/components/food_item.dart';
+import 'package:food_delivery_mobile_app/samples/food_samples.dart';
 
 class HomeContent extends StatelessWidget {
   const HomeContent({super.key});
@@ -9,7 +11,6 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     const spacer = SizedBox(height: 32);
     var backgroundColor = Colors.white;
-    final greyColor = Colors.grey;
 
     return Padding(
       padding: EdgeInsets.all(16.0),
@@ -23,7 +24,30 @@ class HomeContent extends StatelessWidget {
             backgroundColor: WidgetStateProperty.all(backgroundColor),
           ),
           spacer,
-          Category()
+          Category(),
+          spacer,
+          ListTile(
+            title: Text("Popular food", style: TextStyle(
+              fontWeight: FontWeight.bold
+            )),
+            trailing: GestureDetector(
+              onTap: (){
+                // CONTRIBUTIONS : Ouvrir la page de toutes les cards
+              },
+              child: Text("See All", style: TextStyle(
+                color: Colors.grey
+              ),),
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children:
+                foodSamples.map((food){
+                  return FoodItem(food: food);
+                }).toList(),
+            )
+          )
         ],
       )
     );
---
+++ lib/models/food.dart
@@ -0,0 +1,12 @@
+class Food {
+  final String name;
+  final String price;
+  final String url;
+
+
+  Food({
+    required this.name,
+    required this.price,
+    this.url = ''
+  });
+}
