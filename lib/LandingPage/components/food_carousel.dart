6f687cbe2fdc13512301778572283b5b038665da
e-mandy <andymfrd02@gmail.com>
2026-01-02 23:44:55 +0100

    Mise en place de la logique de scroll des repas de la landing page

--- lib/LandingPage/components/food_carousel.dart
+++ lib/LandingPage/components/food_carousel.dart
@@ -1,28 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_mobile_app/LandingPage/models/FoodCarouselItem.dart';
 
-class FoodCarousel extends StatelessWidget {
+class FoodCarousel extends StatefulWidget {
   const FoodCarousel({super.key});
+
+  @override
+  State<FoodCarousel> createState() => _FoodCarouselState();
+}
+
+class _FoodCarouselState extends State<FoodCarousel> {
+  late ScrollController _scrollController;
+  double _scrollOffset = 0.0;
+
+  @override
+  void initState(){
+    super.initState();
+
+    _scrollController = ScrollController();
+    _scrollController.addListener((){
+      setState(() {
+        _scrollOffset = _scrollController.offset;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
     final List<Map<String, String>> FastFoodShows = [
-      {'image': "assets/", "text": "The pizza Burger Pie"},
-      {'image': "assets/", "text": "Le Burger Qui Tue"},
-      {'image': "assets/", "text": "The Pepperoni You Want"},
-      {'image': "assets/", "text": "La Salade Qui Sale Mdr"},
+      {'image': "assets/pizza.jpeg", "text": "The pizza Burger Pie"},
+      {'image': "assets/burger.jpeg", "text": "Le Burger Qui Tue"},
+      {'image': "assets/pepperoni.jpeg", "text": "The Pepperoni You Want"},
+      {'image': "assets/salad.jpeg", "text": "La Salade Qui Sale Mdr"},
     ];
-    return Container(
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        reverse: true,
-        children: [
-          ...FastFoodShows.map((food){
-            return FoodcarouselItem(image: food['image'], text: food['text']);
-          })
-        ],
-      )
+
+    double itemWidth = 250.0;
+    double screenCenter = MediaQuery.of(context).size.width;
+
+    // Pour écouter le scroll des éléments de la liste.
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notification) => true,
+      child: SizedBox(
+        height: 200,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          reverse: true,
+          itemCount: FastFoodShows.length,
+          controller: _scrollController,
+          itemBuilder: (context, index){
+            
+            double itemCenter = (index * itemWidth) + (itemWidth / 2);
+              
+              // Distance entre le centre de l'item et ce que l'utilisateur voit (scroll + milieu écran)
+              double distanceRelative = (itemCenter - (_scrollOffset + screenCenter));
+
+              // On normalise la distance pour que 0 = centre, 1 = un item plus loin à droite
+              double factor = distanceRelative / itemWidth;
+
+            final food = FastFoodShows[index];
+            return FoodcarouselItem(image: food['image']!, text: food['text']!);
+          },
+        )
+      ),
     );
   }
 }
--- lib/LandingPage/models/FoodCarouselItem.dart
+++ lib/LandingPage/models/FoodCarouselItem.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class FoodcarouselItem extends StatelessWidget {
   const FoodcarouselItem({super.key, required this.image, required this.text});
-  final String? image;
-  final String? text;
+  final String image;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +11,19 @@ class FoodcarouselItem extends StatelessWidget {
     return Stack(
       children: [
         Container(
-          width: screenWidth / 3,
+          width: 250,
           decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage("assetName"))
+            image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover)
           )
         ),
         Positioned(
+          width: 50,
           top: 10,
           left: 10,
-          child: Text("data")
+          child: Text(text, style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),)
         )
       ]
     );
