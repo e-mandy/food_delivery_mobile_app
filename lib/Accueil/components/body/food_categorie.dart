0263521334ddb82e3dfcdcb0e05cea37e5048690
e-mandy <andymfrd02@gmail.com>
2026-01-04 03:55:36 +0100

    Composant des catégories - Mise en place de l'UI

--- lib/Accueil/components/body/food_categorie.dart
+++ lib/Accueil/components/body/food_categorie.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
 
-class FoodCategorie extends StatelessWidget {
+class FoodCategorie extends StatefulWidget {
   const FoodCategorie({super.key});
 
   @override
+  State<FoodCategorie> createState() => _FoodCategorieState();
+}
+
+class _FoodCategorieState extends State<FoodCategorie> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final List<String> foodCategories = [
+      "Neapolitan",
+      "Margherita"
+      "Pepperoni",
+      "Quatres Saison",
+      "Royale Pizza"
+    ];
+
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+      child: Row(
+        children: [
+        Expanded(
+          child:
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                spacing: 10,
+                children: [
+                  ...foodCategories.map((category){
+                    return OutlinedButton(
+                      onPressed: (){}, 
+                      style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.black
+                      )
+                    ),
+                    child: Text(category, 
+                      style: 
+                        TextStyle(
+                          color: Colors.black
+                        )
+                    ),
+                    );
+                  }),
+                ],
+              )
+            )
+        )
+      ],),
+    );;
   }
 }
